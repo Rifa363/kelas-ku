2f78697f2493dcfb19fd0029ce2f8d45ee6806dd
Rizal <rf6390343@gmail.com>
2026-06-22 17:38:27 +0700

    fix: auto-delete public/hot on production boot (PHP-level fix)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') !== 'local') {
+        if (app()->environment('production')) {
+            $hotFile = public_path('hot');
+            if (file_exists($hotFile)) {
+                @unlink($hotFile);
+            }
             URL::forceScheme('https');
         }
     }
