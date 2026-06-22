8b77c1cee4848a589b4789ae0256a618716eb994
Rizal <rf6390343@gmail.com>
2026-06-22 19:59:32 +0700

    fix: register Vite as singleton with empty hotFile path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,20 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\Vite;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\Vite as ViteFacade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        if (app()->environment('production')) {
+            $this->app->singleton(Vite::class, function () {
+                return (new Vite)->useHotFile('');
+            });
+        }
     }
 
     public function boot(): void
@@ -21,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
                 @unlink($hotFile);
             }
 
-            Vite::useHotFile('');
+            ViteFacade::useHotFile('');
 
             URL::forceScheme('https');
         }
